6344dff0fe627378f40fa1afad194e163df7fa83
PUT YOUR NAME HEREdwrightdesigns <denise@denisewrightdesigns.com>
2020-06-30 14:12:33 -0500

    First Commit
    Installed create-react-app
    Installed and setup react-router-dom
    Installed and setup SASS
    Installed styled-components
    Installed and setup font-awesome
    Installed react-spring
    Installed framer-motion
    Setup color and font variables in SASS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.scss";
+import { Switch, Route } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Team AMID!</h1>
+    </div>
+  );
+}
+
+export default App;
