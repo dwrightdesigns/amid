b67dec5994eaf4929df44a557fbe8368d165a0fb
mattayoungs <mattayoungs@icloud.com>
2020-07-01 12:58:42 -0400

    started timer page made comp files

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-
+import Timer from "./components/timer/Timer";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello Team AMID!</h1>
+      {/* <Timer /> */}
     </div>
   );
 }
---
+++ src/components/timer/TaskTextInput.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const TextInput = styled.input``;
+
+function TaskTextInput() {
+  return <TextInput></TextInput>;
+}
+
+export default TaskTextInput;
