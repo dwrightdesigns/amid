78f262a848b6ecb0c8067cd61e2203c43eef8d6f
mattayoungs <mattayoungs@icloud.com>
2020-07-02 15:53:40 -0400

    created static choice page and exercise card

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import "./App.scss";
 import { Switch, Route } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 import ChoicePage from "./components/breakChoice/ChoicePage";
+import BreathExCard from "./components/timer/BreathExCard";
 
 function App() {
-  return (
-    <div className="App">
-      <ChoicePage />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
