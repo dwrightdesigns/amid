471800552ce5240e1111e5e598d3a828f6c9b8e4
PUT YOUR NAME HERE <izzyhasten@gmail.com>
2020-07-03 14:43:53 -0400

    Finishing up styling the timer (non-functional)

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,15 +1,29 @@
 import React from "react";
 // import TaskTextInput from "./TaskTextInput";
-// import TimerButton from "./TimerButton";
+import TimerButton from "./TimerButton.js";
 import DigitsTimer from "./DigitsTimer";
 import SkipButton from "./SkipButton";
+import TaskTextArea from "./TaskTextArea";
+import styled from "styled-components";
+
+const TimerFlex = styled.div`
+display: flex;
+align-items: center;
+flex-direction: column;
+background: var(--main-color);
+height: 100vh;
+`;
+
 function Timer() {
   return (
-    <>
+    
+    <TimerFlex>
       <DigitsTimer />
-      {/* <TaskTextInput /> */}
+      <TimerButton/>
+      <TaskTextArea/>
       <SkipButton />
-    </>
+    </TimerFlex>
+
   );
 }
 
