8d1f3d0306229fe2c7e8fe3a37a1979df204027f
mattayoungs <mattayoungs@icloud.com>
2020-07-07 11:56:28 -0400

    turned timer into class

---
+++ src/components/BreathingEx.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const BreatingCont = styled.div``;
+
+function BreathingEx() {
+  return <BreatingCont></BreatingCont>;
+}
+
+export default BreathingEx;
