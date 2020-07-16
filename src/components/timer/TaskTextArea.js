17a5171a1373817b2b92fb8651a3d5a1beb6abac
PUT YOUR NAME HEREdwrightdesigns <denise@denisewrightdesigns.com>
2020-07-16 01:29:08 -0500

    Added local storage to task text area
    On refresh or navigating between pages, the tasktextarea.js will now remember what the user was working on the last time they used the focus timer.

--- src/components/timer/TaskTextArea.js
+++ src/components/timer/TaskTextArea.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const TextAreaStyle = styled.textarea`
@@ -7,12 +7,12 @@ const TextAreaStyle = styled.textarea`
   width: 20rem;
   text-align: center;
   font-family: "Poppins", sans-serif;
-  background-color: rgba(229, 229, 229, .75);
+  background-color: rgba(229, 229, 229, 0.75);
   color: var(--dark-color);
   border-radius: 8px;
   font-size: 1.0625rem;
   font-weight: 400;
-  padding: 1rem 0 0 0;
+  padding: 1rem;
   border: 1px solid var(--main-color);
   @media screen and (min-width: 768px) {
     font-size: 1.375rem;
@@ -24,10 +24,20 @@ const TextAreaStyle = styled.textarea`
 `;
 
 function TaskTextArea() {
+  const currentTask = localStorage.getItem("task");
+  const [task, setTask] = useState(currentTask);
+
+  const handleChange = (e) => {
+    localStorage.setItem("task", e.target.value);
+    setTask(e.target.value);
+  };
+
   return (
     <TextAreaStyle
       placeholder="What are you working on?"
       rows="4"
+      value={task}
+      onChange={handleChange}
     ></TextAreaStyle>
   );
 }
