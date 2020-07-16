ccc300cfa201401d88afbb6bb48e20b2862d0d76
Amanda Key <ariaandezrasmom@gmail.com>
2020-07-16 10:50:12 -0500

    added and styled remaining content for about page

--- src/components/About.js
+++ src/components/About.js
@@ -40,6 +40,7 @@ const StyledHero = styled.section`
 
   img {
       width: 20rem;
+      padding-bottom: 1rem;
   }
 `;
 const StyledPage = styled.div`
@@ -63,6 +64,7 @@ background-size: cover;
 @media screen and (min-width: 1024px) {
   padding: 10rem 8rem;
   flex-direction: row;
+  flex-flow: wrap;
   margin: auto;
 }
 
@@ -85,37 +87,95 @@ background-size: cover;
 
 img {
     width: 20rem;
+    margin-bottom: 1em;
+    border-style: solid;
+    border-width: .2rem;
+    border-radius: 1em;
+    border-color: var(--dark-color);
+}
+h5 {
+  margin-bottom: 1em;
+  text-align: center;
+}
+cite {
+  padding: 1rem;
+}
+`;
+
+const StyledScope = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: space-evenly;
+color: var(--dark-color);
+align-items: center;
+width: 100%;
+background: linear-gradient(
+  180.03deg,
+  rgba(255, 255, 255, 0.5) 15.67%,
+  rgba(255, 255, 255, 0.5) 144.67%
+),
+url(img/beach.jpg);
+background-position: center;
+background-repeat: no-repeat;
+background-size: cover;
+padding: 1rem 1rem;
+@media screen and (min-width: 768px) {
+  padding: 5rem 4rem;
+}
+@media screen and (min-width: 1024px) {
+  padding: 10rem 8rem;
+  flex-direction: row;
+  margin: auto;
 }
 `;
 
 
 
 function About() {
-    return (
-        <StyledPage>
-            <StyledHero>
-                <div className="about-amid">
-                    <h1>About</h1>
-                    <h4>
-                        A pomodoro timer which focuses on the breaks rather than the tasks.
-                        It creates moments of mindfulness throughout the day to give you
-                        time that might otherwise not be achievable.
+  return (
+    <StyledPage>
+      <StyledHero>
+        <div className="about-amid">
+          <h1>About</h1>
+          <h4>
+            A pomodoro timer which focuses on the breaks rather than the tasks.
+            It creates moments of mindfulness throughout the day to give you
+            time that might otherwise not be achievable.
           </h4>
-                </div>
-                <img src="/img/logo.svg" alt="" />
-            </StyledHero>
-            <StyledTeam>
-                <div className="team">
-                    <h1>Our Team</h1>
-                    </div>
-                        <img src="/img/Denise.jpg"/>
-                    
-
-
-                
-            </StyledTeam>
-        </StyledPage>
-    )
+        </div>
+        <img alt="amid logo" src="/img/logo.svg" />
+      </StyledHero>
+      
+      <StyledTeam>
+        <div className="team">
+          <h1>The Team</h1>
+        </div>
+        <div>
+        <img alt="Denise" src="/img/Denise.jpg" />
+        <h5>Denise Wright</h5>
+        </div>
+        <div>
+        <img alt="Matt" src="/img/Matt.jpg" />
+        <h5>Matt Youngs</h5>
+        </div>
+        <div>
+        <img alt="Izzy" src="/img/Izzy.jpg" />
+        <h5>Jacob "Izzy" Hasten</h5>
+        </div>
+        <div>
+        <img alt="Amanda" src="/img/Amanda.jpg" />
+        <h5>Amanda Key</h5>
+        </div>
+      </StyledTeam>
+      <StyledScope>
+        <div className="scope">
+          <h1>The Scope</h1>
+          <p>This project had a 4-week deadline. In the scope of that month, the team had to research, design, test, engineer and deploy the most valuable proponents of the application, making sure they did their best to confront the problems of their users.</p>
+        </div>
+      </StyledScope>
+      <cite>created by team amid</cite>
+    </StyledPage>
+  )
 }
 
 export default About;
